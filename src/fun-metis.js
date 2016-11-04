fcf06f9d05a608fe87166e27f328865fc19b5854
funtom48 <ivan40in48@gmail.com>
2016-11-04 17:20:57 +0300

    change deploy file

---
+++ src/fun-metis.js
@@ -0,0 +1,31 @@
+(function (angular) {
+    'use strict';
+    /*
+        Metis menu directive for AngularJS
+        author: funtom48
+    */
+    var MetisMenuDirective = function ($timeout) {
+        return {
+            restrict: 'E',
+            scope: {
+                config: '=funMetisConfig',
+                events: '=funMetisEvents'
+            },
+            link: function (scope, element) {
+                var menuElement = element.find('> nav > ul');
+                menuElement.addClass('metismenu');
+                var config = scope.config || {};
+                var events = scope.events || {};
+                $timeout(function () {
+                    var menu = menuElement.metisMenu(config);
+                    angular.forEach(events, function (event) {
+                        menu.on(event.type, event.callback);
+                    });
+                });
+            }
+        };
+    };
+
+    angular.module('funMetisMenu', [])
+        .directive('funMetisMenu', ['$timeout', MetisMenuDirective]);
+})(angular);
