72bd7eee04ee99807716f9dd562367353d41fc18
funtom48 <ivan40in48@gmail.com>
2019-10-21 00:37:41 +0300

    :sparkles: update demo

--- demo/demo.js
+++ demo/demo.js
@@ -1,12 +1,17 @@
-angular.module('myApp', ['funMetisMenu'])
-.controller('MyCtrl', function($scope) {
-    $scope.metis = {
-        config: {
-            toggle: true,
-        },
-        events: [{
-            type: 'hide.metisMenu',
-            callback: (event) => console.log("423523523", event)
-        }]
-    };
-});
+angular
+    .module('myApp', ['funMetisMenu'])
+    .controller('MyCtrl', function($scope) {
+        $scope.items = [
+            ["first", ["test1", "test2", "test3"]],
+            ["second", ["test1", "test2", "test3"]],
+        ];
+        $scope.metis = {
+            config: {
+                toggle: true,
+            },
+            events: [{
+                type: 'hide.metisMenu',
+                callback: (event) => console.log("event", event)
+            }]
+        };
+    });
